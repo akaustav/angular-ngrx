88ba412e0e8fb2bca113908c857ea395e9d96ca5
Ameet Kaustav <Ameet.Kaustav@matrixhealth.net>
2019-09-19 16:06:13 -0700

    [+] Wiring modules into AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { appReducers } from './store/reducers/app.reducers';
+import { UserEffects } from './store/effects/user.effects';
+import { ConfigEffects } from './store/effects/config.effects';
+import { UserService } from './services/user.service';
 
 @NgModule({
   declarations: [
@@ -10,9 +19,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    StoreModule.forRoot(appReducers),
+    EffectsModule.forRoot([UserEffects, ConfigEffects]),
+    StoreRouterConnectingModule.forRoot({ stateKey: 'router' }),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
     AppRoutingModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
