021212ec25506771348eedc51bf9528f982c1dd9
Ameet Kaustav <Ameet.Kaustav@matrixhealth.net>
2019-09-19 16:21:00 -0700

    [+] App Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+import { IAppState } from './store/state/app.state';
+import { GetConfig } from './store/actions/config.actions';
+import { selectConfig } from './store/selectors/config.selectors';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-ngrx';
+  config$ = this.store.pipe(select(selectConfig));
+
+  constructor(private store: Store<IAppState>) { }
+
+  ngOnInit() {
+    this.store.dispatch(new GetConfig());
+  }
 }
